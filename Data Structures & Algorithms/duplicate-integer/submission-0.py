ce77b3b1ab706e59b524b8f11eef8907a8fc74a4
Aishwarya Chaudhari <138247881+Octopush18@users.noreply.github.com>
2026-04-07 20:05:59 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        s = sorted(nums)
+        for i in range(len(s)-1):
+            a = s[i]
+            # print("this is a ",a)
+            if a != s[i+1]:
+                continue
+            else:
+                return True
+        return False
